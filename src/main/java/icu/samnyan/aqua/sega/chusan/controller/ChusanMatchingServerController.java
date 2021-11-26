2c6d96d7653d5c8b37bd154dbcdbbfb63123ed32
Lawrence <Lawrence03@users.noreply.github.com>
2021-11-26 04:09:39 -0800

    fixes

---
+++ src/main/java/icu/samnyan/aqua/sega/chusan/controller/ChusanMatchingServerController.java
@@ -0,0 +1,21 @@
+package icu.samnyan.aqua.sega.chusan.controller;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+/**
+ * todo
+ */
+@RestController
+@RequestMapping("/ChusanServlet/MatchingServer")
+public class ChusanMatchingServerController {
+
+    @PostMapping(value = "Ping")
+    String getGameCharge(@ModelAttribute Map<String, Object> request) {
+        return "{\"returnCode\":\"-1\"}";
+    }
+}
--- src/main/java/icu/samnyan/aqua/sega/chusan/handler/impl/GetGameSettingHandler.java
+++ src/main/java/icu/samnyan/aqua/sega/chusan/handler/impl/GetGameSettingHandler.java
@@ -31,12 +31,24 @@ public class GetGameSettingHandler implements BaseHandler {
 
     private final String PORT;
 
+    private final String romVersion;
+
+    private final String dataVersion;
+
     @Autowired
-    public GetGameSettingHandler(StringMapper mapper, PropertyEntryRepository propertyEntryRepository, @Value("${allnet.server.host}") String HOST, @Value("${allnet.server.port}") String PORT) {
+    public GetGameSettingHandler(StringMapper mapper,
+                                 PropertyEntryRepository propertyEntryRepository,
+                                 @Value("${allnet.server.host}") String HOST,
+                                 @Value("${allnet.server.port}") String PORT,
+                                 @Value("${game.chusan.rom-version}") String romVersion,
+                                 @Value("${game.chusan.data-version}") String dataVersion
+    ) {
         this.mapper = mapper;
         this.propertyEntryRepository = propertyEntryRepository;
         this.HOST = HOST;
         this.PORT = PORT;
+        this.romVersion = romVersion;
+        this.dataVersion = dataVersion;
     }
 
 
@@ -44,13 +56,13 @@ public class GetGameSettingHandler implements BaseHandler {
     public String handle(Map<String, Object> request) throws JsonProcessingException {
 
         PropertyEntry start = propertyEntryRepository.findByPropertyKey("reboot_start_time")
-                .orElseGet(() -> new PropertyEntry("reboot_start_time", "2020-01-01 06:30:00.0"));
+                .orElseGet(() -> new PropertyEntry("reboot_start_time", "2020-01-01 23:59:00.0"));
         PropertyEntry end = propertyEntryRepository.findByPropertyKey("reboot_end_time")
-                .orElseGet(() -> new PropertyEntry("reboot_end_time", "2020-01-01 07:00:00.0"));
+                .orElseGet(() -> new PropertyEntry("reboot_end_time", "2020-01-01 23:59:00.0"));
 
         GameSetting gameSetting = new GameSetting(
-                "2.01.00",
-                "2.00.01",
+                romVersion,
+                dataVersion,
                 false,
                 10,
                 start.getPropertyValue(),
